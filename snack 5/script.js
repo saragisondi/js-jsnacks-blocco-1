e0f316dcf7e208a3b322cc5c34e89bb97d0e407d
Sara Gisondi <sara.gisondi@outlook.it>
2023-03-27 14:35:52 +0200

    add prompt

---
+++ snack 5/script.js
@@ -0,0 +1,11 @@
+//CREO UN ARRAY VUOTO
+let numberArray = [];
+
+
+//FACCIO UN CICLO E UN PROMPT PER CHIEDERE ALL'UTENTE UN NUMERO
+for (let i = 0 ; i < 6 ; i++){
+  let number = prompt('inserisci un numero')
+  console.log(number[i])
+}
+
+//
